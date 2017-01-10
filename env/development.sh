68aecbb8c1c88c12af09d3560bea7b28ecaacac5
Jacob Moeller <jacobmoe@gmail.com>
2017-01-09 20:06:30 -0500

    setup config

---
+++ env/development.sh
@@ -0,0 +1,3 @@
+export NODE_ENV=development
+
+source ./secrets.sh
--- src/api/congress/routes.js
+++ src/api/congress/routes.js
@@ -14,5 +14,4 @@ router.get('/senate', senate())
 
 app.use(router.routes()).use(router.allowedMethods())
 
-
 module.exports = app
