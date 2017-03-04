2ec5620758e5be60035b81b1e1ab77d00d11d137
Jacob Moeller <jacobmoe@gmail.com>
2017-03-04 15:48:41 -0500

    login route

--- src/users/auth/controller.js
+++ src/users/auth/controller.js
@@ -3,16 +3,20 @@ const config = require('config')
 
 const User = require('../model')
 
+function generateToken (user) {
+  return jwt.sign(user.prepare(), config.get('jwt.secret'))
+}
+
 function register () {
   return async (ctx) => {
     try {
       const user = await User.create(ctx.request.body)
-      const payload = user.prepare()
-      const token = jwt.sign(payload, config.get('jwt.secret'))
+      const token = generateToken(user)
 
       ctx.status = 201
       ctx.body = {
-        token
+        token,
+        user: user.prepare()
       }
     } catch (err) {
       ctx.status = 400
@@ -23,17 +27,29 @@ function register () {
 
 function login () {
   return async (ctx) => {
+    const email    = ctx.request.body.email || null
+    const password = ctx.request.body.password
 
-  }
-}
-
-function logout () {
-  return async (ctx) => {
+    try {
+      const user = await User.find({email: email})
+      if (user && await (user.validatePassword(password))) {
+        ctx.status = 200
+        ctx.body = {
+          token: generateToken(user),
+          user: user.prepare()
+        }
+      } else {
+        ctx.status = 401
+        ctx.body = { error: 'email or password does not match' }
+      }
+    } catch (err) {
+      ctx.status = 400
+      ctx.body = err
+    }
   }
 }
 
 module.exports = {
   register,
-  login,
-  logout
+  login
 }
--- test/users/auth/controller.js
+++ test/users/auth/controller.js
@@ -2,6 +2,7 @@ const { describe, it, beforeEach, afterEach } = require('mocha')
 const { assert } = require('chai')
 const config = require('config')
 const controller = require('../../../src/users/auth/controller')
+const User = require('../../../src/users/model')
 const { truncate } = require('../../helpers')
 const table = require('../../../db/orm/tables/users')
 const jwt = require('jsonwebtoken')
@@ -28,12 +29,81 @@ describe('users/auth/controller', () => {
       const registerAction = controller.register()
       await registerAction(ctx)
 
-      const user = jwt.verify(ctx.body.token, config.get('jwt.secret'))
+      const user = ctx.body.user
 
+      assert.isString(ctx.body.token)
       assert.equal(user.id, 1)
       assert.equal(user.email, 'test@example.com')
       assert.equal(user.name, 'boop')
       assert.equal(ctx.status, 201)
+    })
+  })
+
+  describe('login', () => {
+    it('checks email/password and returns a token', async () => {
+      const email = 'test@example.com'
+      const password = 'passpass'
+      const name = 'biff'
+
+      const user = await User.create({
+        password: password,
+        email: email,
+        name: name
+      })
+
+      let ctx = {
+        body: null,
+        status: null,
+        request: {
+          body: {
+            password: password,
+            email: email
+          }
+        }
+      }
+
+      const loginAction = controller.login()
+      await loginAction(ctx)
+
+      assert.deepEqual(ctx.body.user, user.prepare())
+      assert.isString(ctx.body.token)
+      assert.equal(ctx.status, 200)
+
+      ctx = {
+        body: null,
+        status: null,
+        request: {
+          body: {
+            password: 'wrong password',
+            email: email
+          }
+        }
+      }
+
+      await loginAction(ctx)
+
+      assert.isUndefined(ctx.body.user)
+      assert.isUndefined(ctx.body.token)
+      assert.isString(ctx.body.error)
+      assert.equal(ctx.status, 401)
+
+      ctx = {
+        body: null,
+        status: null,
+        request: {
+          body: {
+            password: 'wrong password',
+            email: 'not an email'
+          }
+        }
+      }
+
+      await loginAction(ctx)
+
+      assert.isUndefined(ctx.body.user)
+      assert.isUndefined(ctx.body.token)
+      assert.isString(ctx.body.error)
+      assert.equal(ctx.status, 401)
     })
   })
 })
