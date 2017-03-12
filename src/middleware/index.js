2fa27c676ebd9de67381705f8655f23fb42d1919
Jacob Moeller <jacobmoe@gmail.com>
2017-03-12 14:28:02 -0400

    cors come first :(

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,7 +1,7 @@
 module.exports = (app) => {
+  app.use(require('./cors'))
   app.use(require('./bodyparser')())
   app.use(require('./errors')())
   app.use(require('./logger')())
   app.use(require('./auth'))
-  app.use(require('./cors'))
 }
